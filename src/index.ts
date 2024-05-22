1cee05e0392a62deaa2a31494ac18f4e9df51a34
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2024-05-22 11:35:27 -0500

    feat: env vars

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get("/", (request: Request, response: Response) => { 
   console.log('get request received');
@@ -10,5 +10,6 @@ app.get("/", (request: Request, response: Response) => {
 });
  
 app.listen(PORT, () => { 
-  console.log("Running at PORT ", PORT); 
+  console.log("Running at PORT ", PORT);
+  console.log(process.env)
 });
