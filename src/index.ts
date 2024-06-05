cd20559b76c99d087554d02a509775373a7a995f
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2024-06-05 12:23:52 -0500

    feat: cleaning index

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,9 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.get("/", (request: Request, response: Response) => { 
-  console.log('get request received');
   response.status(200).send("Hello World");
-  console.log('get request responded');
 });
  
 app.listen(PORT, () => { 
   console.log("Running at PORT ", PORT);
-  console.log(process.env)
 });
