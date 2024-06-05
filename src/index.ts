e5a9fe2b41d6142d89afc4d6e76cff4223a04670
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2024-06-05 12:17:53 -0500

    feat: doppler sdk

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import express, { Request, Response } from "express";
-const app = express();
+import DopplerSDK from '@dopplerhq/node-sdk';
 
-const PORT = process.env.PORT;
+interface Env {
+  PORT: string;
+}
 
-app.get("/", (request: Request, response: Response) => { 
-  console.log('get request received');
-  response.status(200).send("Hello World");
-  console.log('get request responded');
-});
- 
-app.listen(PORT, () => { 
-  console.log("Running at PORT ", PORT);
-  console.log(process.env)
-});
+const initEnvironment = async () : Promise<Env>  => {
+  const doppler = new DopplerSDK({ accessToken: 'TU_TOKEN_AQUI' });
+  const PORT = await doppler.secrets.get('backend-express', 'dev', 'PORT')
+  return {
+    PORT: PORT.value?.raw as string
+  }
+}
+
+const initApp = async (env: Env) => {
+  const app = express();
+
+
+  app.get("/", (request: Request, response: Response) => { 
+    console.log('get request received');
+    response.status(200).send("Hello World");
+    console.log('get request responded');
+  });
+  
+   
+  app.listen(env.PORT, async () => { 
+    console.log("Running at PORT ", env.PORT);
+  });
+}
+
+
+const main = async () => {
+  const env = await initEnvironment();
+  await initApp(env);
+}
+
+main();
+
+
