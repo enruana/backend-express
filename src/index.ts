ec20fd52e104b5cdcdd14631f9649e66438e07ca
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2024-06-05 13:55:50 -0500

    feat: doppler cli integration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
+import { config } from "./helpers/config";
+
 const app = express();
 
-const PORT = process.env.PORT;
-
-app.get("/", (request: Request, response: Response) => { 
+app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
 });
- 
-app.listen(PORT, () => { 
-  console.log("Running at PORT ", PORT);
+
+app.listen(config.PORT, () => {
+  console.log("Service running on:", config.PORT);
 });
